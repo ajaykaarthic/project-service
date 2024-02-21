893fe2996d44524c82e1039405d212cc70a3477c
Ajay Kaarthic Jeysree <ajay.kaarthic@gmail.com>
2024-02-20 18:01:21 -0600

    fix bug

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
 import express from "express";
 import http from "http";
 import router from "./router";
@@ -7,6 +9,14 @@ import router from "./router";
 // Creating an Express application instance
 const app = express();
 
+app.use(
+  cors({
+    credentials: true,
+    origin: `http://localhost:3000`,
+  })
+);
+
+app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
@@ -14,7 +24,7 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 
 server.listen(3001, () => {
-  console.log(`server started on http://localhost:/3001`);
+  console.log("server started on http://localhost:/3001");
 });
 
 app.use("/", router());
